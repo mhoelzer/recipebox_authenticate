dbaf47c059cd0f3fa3b840f080280531bae91934
Lea Ellingwood <leaellingwood@hotmail.com>
2019-05-08 09:39:26 -0400

    added buttons

--- recipebox/settings.py
+++ recipebox/settings.py
@@ -41,8 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'recipebox',
+    'crispy_forms',
     
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
